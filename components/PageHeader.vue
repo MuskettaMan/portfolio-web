1fb810311b1c85c0722e6155c6f3ae4d5b072b25
MuskettaMan <ev.fdl.ve@gmail.com>
2025-02-05 17:24:36 +0100

    Added model loading and threejs, updated styling

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,8 +1,13 @@
 <template>
 	<header class="page-header">
-		<p>Hi, my name is</p>
-		<h1>Ferri de Lange</h1>
-		<p>I enjoy low-level graphics and game development</p>
+		<div class="intro">
+			<p>Hi, my name is</p>
+			<h1>Ferri de Lange</h1>
+			<p>I enjoy low-level graphics and game development</p>
+		</div>
+		<div class="model">
+			<!--			<NiceModel/>-->
+		</div>
 	</header>
 </template>
 
@@ -22,35 +27,51 @@ export default {
 
 <style scoped lang="scss">
 .page-header {
-	margin: 0 10rem 0;
 	display: flex;
-	flex-direction: column;
-	gap: 1rem;
-	height: 40vh;
-	justify-content: center;
-	position: relative;
+	justify-content: space-between;
+	margin: 0 10rem 0;
+	min-height: 40vh;
 
-	p {
-		font-size: 2rem;
-		margin: 0;
-	}
-
-	h1 {
-		font-size: 6rem;
-	}
-}
-
-@media only screen and (max-width: 1000px) {
-	.page-header {
-		margin: 0 2rem 0;
-		height: 30vh;
+	.intro {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		justify-content: center;
+		position: relative;
+		width: 70%;
 
 		p {
-			font-size: 1.6rem;
+			font-size: 2rem;
+			margin: 0;
 		}
 
 		h1 {
-			font-size: 4.0rem;
+			font-size: 6rem;
+		}
+	}
+
+	.model {
+		width: 30%;
+	}
+}
+
+@media only screen and (max-width: 1100px) {
+	.page-header {
+		margin: 0 2rem 0;
+		min-height: 30vh;
+
+		.intro {
+			p {
+				font-size: 1.6rem;
+			}
+
+			h1 {
+				font-size: 4.0rem;
+			}
+		}
+
+		.model {
+			height: 20rem;
 		}
 	}
 }
@@ -58,14 +79,20 @@ export default {
 @media only screen and (max-width: 600px) {
 	.page-header {
 		margin: 0 2rem 0;
-		height: 30vh;
+		flex-direction: column;
 
-		p {
-			font-size: 1.2rem;
+		.intro {
+			p {
+				font-size: 1.2rem;
+			}
+
+			h1 {
+				font-size: 3.0rem;
+			}
 		}
 
-		h1 {
-			font-size: 3.0rem;
+		.model {
+			width: 100%;
 		}
 	}
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,65 +2,69 @@ import slugify from "slugify";
 import axios from "axios";
 
 export default defineNuxtConfig({
-  content: {
-      highlight: {
-          theme: 'one-dark-pro',
-          preload: [
-              'cpp', 'wgsl', 'cmake'
-          ]
-      }
-  },
+    content: {
+        highlight: {
+            theme: 'one-dark-pro',
+            preload: [
+                'cpp', 'wgsl', 'cmake'
+            ]
+        }
+    },
 
-  devtools: {enabled: true},
+    devtools: {enabled: true},
 
-  css: [
-      '@fortawesome/fontawesome-svg-core/styles.css'
-  ],
+    css: [
+        '@fortawesome/fontawesome-svg-core/styles.css'
+    ],
 
-  vite: {
-      server: {
-          fs: {
-              allow: ['.'] // Allows access to the root directory
-          }
-      },
-      css: {
-          preprocessorOptions: {
-              scss: {
-                  additionalData: '@use "~/assets/_globalStyle.scss" as *;\n'
-              }
-          }
-      }
-  },
+    vite: {
+        server: {
+            fs: {
+                allow: ['.'] // Allows access to the root directory
+            }
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "~/assets/_globalStyle.scss" as *;\n'
+                }
+            }
+        }
+    },
 
-  modules: [
-      '@pinia/nuxt', '@nuxt/content'
-  ],
+    modules: [
+        '@pinia/nuxt', '@nuxt/content', '@tresjs/nuxt'
+    ],
 
-  build: {
-      transpile: ['@fortawesome/vue-fontawesome']
-  },
+    tres: {
+        devtools: true
+    },
 
-  ssr: true,
+    build: {
+        transpile: ['@fortawesome/vue-fontawesome']
+    },
 
-  hooks: {
-      async 'nitro:config'(nitroConfig) {
-          const articles = (await axios.get('https://ferri.dev/api/articles')).data.data;
-          const projects = (await axios.get('https://ferri.dev/api/projects')).data.data;
+    ssr: true,
 
-          const articleSlugs = articles.map(article => `/article/${slugify(article.title, {lower: true})}`);
-          const projectSlugs = projects.map(project => `/project/${slugify(project.title, {lower: true})}`);
-          nitroConfig.prerender.routes.push(...articleSlugs);
-          nitroConfig.prerender.routes.push(...projectSlugs);
-      }
-  },
+    hooks: {
+        async 'nitro:config'(nitroConfig) {
+            const articles = (await axios.get('https://ferri.dev/api/articles')).data.data;
+            const projects = (await axios.get('https://ferri.dev/api/projects')).data.data;
 
-  app: {
-      head: {
-          charset: 'utf-8',
-          viewport: 'width=device-width, initial-scale=1',
-          title: 'Ferri.dev'
-      }
-  },
+            const articleSlugs = articles.map(article => `/article/${slugify(article.title, {lower: true})}`);
+            const projectSlugs = projects.map(project => `/project/${slugify(project.title, {lower: true})}`);
+            nitroConfig.prerender.routes.push(...articleSlugs);
+            nitroConfig.prerender.routes.push(...projectSlugs);
+        }
+    },
 
-  compatibilityDate: '2025-01-27'
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: 'Ferri.dev'
+        }
+    },
+
+    compatibilityDate: '2025-01-27'
 })
